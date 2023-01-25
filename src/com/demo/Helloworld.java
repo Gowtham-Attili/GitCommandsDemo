343d6538e757364044c920496538b9bc98d0bb8e
Gowtham <gowtham.attili@nexturn.com>
2023-01-25 12:30:39 +0530

    "gggg"

---
+++ src/com/demo/Helloworld.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class Helloworld {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println(c);
+    }
+}
